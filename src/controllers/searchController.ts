210099816d37e26a94ac9b345fb1e6d25710d09f
Lívia Montes <l.montes@aluno.ifsp.edu.br>
2026-03-25 08:40:20 -0300

    site finalizado

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,6 +1,19 @@
 import { Request, Response } from 'express';
+import { Pet } from '../models/pet'
+import { createMenuObject } from '../helpers/createMenuObject';
 
 export const search = (req: Request, res: Response) => {
+	let query: string = req.query.q as string
+	if(!query) {
+		res.redirect('/')
+
+		return
+	}
+	let list = Pet.getFromName(query)
 	res.send('Buscando.....');
-	// res.render('pages/search');
+	res.render('pages/pages', {
+		menu: createMenuObject(''),
+		list,
+		query
+	});
 }
